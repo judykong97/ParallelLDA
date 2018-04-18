5920e464724b3e92e04aa1c2cc2351197846eada
judykong97 <junhank@andrew.cmu.edu>
2018-04-17 20:54:31 -0400

    Fixed minor indentation issues

--- lda.cpp
+++ lda.cpp
@@ -14,11 +14,10 @@ using namespace std;
  */
 template <typename T>
 vector<size_t> sort_indexes(const vector<T> &v) {
-  vector<size_t> idx(v.size());
-  iota(idx.begin(), idx.end(), 0);
-  sort(idx.begin(), idx.end(),
-       [&v](size_t i1, size_t i2) {return v[i1] > v[i2];});
-  return idx;
+    vector<size_t> idx(v.size());
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(),[&v](size_t i1, size_t i2) {return v[i1] > v[i2];});
+    return idx;
 }
 
 inline double logDirichlet_const(double alpha, int k) {
@@ -28,7 +27,7 @@ inline double logDirichlet_const(double alpha, int k) {
 double logDirichlet_vector(vector<double> alpha) {
     double sumLogGamma = 0.0;
     double logSumGamma = 0.0;
-    for(int i=0; i<alpha.size(); i++) {
+    for(int i = 0; i < alpha.size(); i++) {
         sumLogGamma += lgamma(alpha[i]);
         logSumGamma += alpha[i];
     }
@@ -41,16 +40,16 @@ double getLogLikelihood(vector<vector<int>> wordTopicTable, vector<vector<int>> 
     int numDocs = docTopicTable.size();
     int numTopics = docTopicTable[0].size();
     vector<double> temp(numWords, 0.0);
-    for(int k=0; k<numTopics; k++) {
-        for(int w=0; w<numWords; w++) {
+    for (int k = 0; k < numTopics; k++) {
+        for (int w = 0; w < numWords; w++) {
             temp[w] = beta + wordTopicTable[w][k];
         }
         lik += logDirichlet_vector(temp);
         lik -= logDirichlet_const(beta, numWords);
     }
     vector<double> temp2(numTopics, 0.0);
-    for(int d=0; d<numDocs; d++) {
-        for(int k=0; k<numTopics; k++) {
+    for (int d = 0; d < numDocs; d++) {
+        for (int k = 0; k < numTopics; k++) {
             temp2[k] = alpha + docTopicTable[d][k];
         }
         lik += logDirichlet_vector(temp2);
@@ -60,38 +59,38 @@ double getLogLikelihood(vector<vector<int>> wordTopicTable, vector<vector<int>> 
 }
 
 void runLDA(vector<vector<int>> w, vector<vector<int>> &z, 
-	int numDocs, int numWords, int numTopics, double alpha, double beta, int numIterations) {
+        int numDocs, int numWords, int numTopics, double alpha, double beta, int numIterations) {
 
-  clock_t start;
-  double duration;
+    clock_t start;
+    double duration;
 
-	vector<vector<int>> docTopicTable(numDocs, vector<int>(numTopics, 0));
-	vector<vector<int>> wordTopicTable(numWords, vector<int>(numTopics, 1));
-  	vector<int> topicTable(numTopics, 0);
+    vector<vector<int>> docTopicTable(numDocs, vector<int>(numTopics, 0));
+    vector<vector<int>> wordTopicTable(numWords, vector<int>(numTopics, 1));
+    vector<int> topicTable(numTopics, 0);
     vector<double> p(numTopics, 0.0);
 
-  	for (int i = 0; i < numDocs; i++) { // numDocs = w.size();
-  		vector<int> z_line(w[i].size(), -1);
-  		z.push_back(z_line);
-  	}
+    for (int i = 0; i < numDocs; i++) { // numDocs = w.size();
+        vector<int> z_line(w[i].size(), -1);
+        z.push_back(z_line);
+    }
 
-  	for (int d = 0; d < numDocs; d++) {
-  		for (int j = 0; j < w[d].size(); j++) {
-  			int word = w[d][j];
-  			int topic = rand() % numTopics;
-  			z[d][j] = topic;
-  			docTopicTable[d][topic]++;
-  			wordTopicTable[word][topic]++;
-  			topicTable[topic]++;
-  		}
-  	}
+    for (int d = 0; d < numDocs; d++) {
+        for (int j = 0; j < w[d].size(); j++) {
+            int word = w[d][j];
+            int topic = rand() % numTopics;
+            z[d][j] = topic;
+            docTopicTable[d][topic]++;
+            wordTopicTable[word][topic]++;
+            topicTable[topic]++;
+        }
+    }
 
-    for(int i = 0; i < numIterations; i++) {
+    for (int i = 0; i < numIterations; i++) {
 
         start = clock();
 
-        for(int d = 0; d < numDocs; d++) {
-            for(int j = 0; j < w[d].size(); j++) {
+        for (int d = 0; d < numDocs; d++) {
+            for (int j = 0; j < w[d].size(); j++) {
                 int word = w[d][j];
                 int topic = z[d][j];
                 docTopicTable[d][topic]--;
@@ -100,7 +99,7 @@ void runLDA(vector<vector<int>> w, vector<vector<int>> &z,
 
                 double norm = 0.0;
                 int newk = topic;
-                for(int k = 0; k < numTopics; k++) {
+                for (int k = 0; k < numTopics; k++) {
                     int z_dj_equals_k = (k == topic);
                     double ak = docTopicTable[d][k] - z_dj_equals_k + alpha;
                     double bk = (wordTopicTable[word][k] - z_dj_equals_k + beta) / (topicTable[k] - z_dj_equals_k + numWords + beta);
@@ -162,20 +161,20 @@ void runLDA(vector<vector<int>> w, vector<vector<int>> &z,
     string line;
     while (getline(infile, line))
     {
-      vocab.push_back(line);
+        vocab.push_back(line);
     }
     vector<vector<int>> outputTable(wordTopicTable[0].size(), vector<int>(numWords));
     for (int i = 0; i < numWords; i++) {
-      for (int j = 0; j < wordTopicTable[0].size(); j++) {
-        outputTable[j][i] = wordTopicTable[i][j];
-      }
+        for (int j = 0; j < wordTopicTable[0].size(); j++) {
+            outputTable[j][i] = wordTopicTable[i][j];
+        }
     }
     for (int t = 0; t < numTopics; t++) {
-      vector<size_t> v = sort_indexes(outputTable[t]);
-      for (int w = 0; w < 5; w++) {
-        cout << vocab[v[w]] << " ";
-      }
-      cout << endl;
+        vector<size_t> v = sort_indexes(outputTable[t]);
+        for (int w = 0; w < 5; w++) {
+            cout << vocab[v[w]] << " ";
+        }
+        cout << endl;
     }
 
     // Output duration of training
