2ef212e8eaa170eee91b97327870351df8f223be
junhank <junhank@andrew.cmu.edu>
2018-04-26 23:19:44 -0400

    Naive working version of MPI

--- lda.cpp
+++ lda.cpp
@@ -6,6 +6,7 @@
 #include <numeric>
 #include <cmath>
 #include <ctime>
+#include <string.h>
 
 
 using namespace std;
@@ -35,23 +36,24 @@ double logDirichlet_vector(vector<double> alpha) {
     return sumLogGamma - lgamma(logSumGamma);
 }
 
-double getLogLikelihood(vector<vector<int>> wordTopicTable, vector<vector<int>> docTopicTable, double alpha, double beta) {
+double getLogLikelihood(int* wordTopicTable, int* docTopicTable, double alpha, double beta, int numWords, int numDocs, int numTopics) {
     double lik = 0.0;
-    int numWords = wordTopicTable.size();
-    int numDocs = docTopicTable.size();
-    int numTopics = docTopicTable[0].size();
+    // int numWords = wordTopicTable.size();
+    // int numDocs = docTopicTable.size();
+    // int numTopics = docTopicTable[0].size();
     vector<double> temp(numWords, 0.0);
     for (int k = 0; k < numTopics; k++) {
         for (int w = 0; w < numWords; w++) {
-            temp[w] = beta + wordTopicTable[w][k];
+            temp[w] = beta + wordTopicTable[w * numTopics + k];
         }
         lik += logDirichlet_vector(temp);
         lik -= logDirichlet_const(beta, numWords);
     }
     vector<double> temp2(numTopics, 0.0);
     for (int d = 0; d < numDocs; d++) {
+        int offset = d * numTopics;
         for (int k = 0; k < numTopics; k++) {
-            temp2[k] = alpha + docTopicTable[d][k];
+            temp2[k] = alpha + docTopicTable[offset + k];
         }
         lik += logDirichlet_vector(temp2);
         lik -= logDirichlet_const(alpha, numTopics);
@@ -59,29 +61,51 @@ double getLogLikelihood(vector<vector<int>> wordTopicTable, vector<vector<int>> 
     return lik;
 }
 
-void runLDA(vector<vector<int>> w, vector<vector<int>> &z, 
-        int numDocs, int numWords, int numTopics, double alpha, double beta, int numIterations) {
+void runLDA(int *w, int *w_start, 
+        int numDocs, int numWords, int numTopics, double alpha, double beta, int numIterations, int process_id, int process_count) {
 
     clock_t start;
     double duration;
 
-    vector<vector<int>> docTopicTable(numDocs, vector<int>(numTopics, 0));
-    vector<vector<int>> wordTopicTable(numWords, vector<int>(numTopics, 1));
-    vector<int> topicTable(numTopics, 0);
-    vector<double> p(numTopics, 0.0);
+    int* z = (int*) calloc(TOTAL_WORDS, sizeof(int));
+    int* docTopicTable = (int*) calloc(numDocs * numTopics, sizeof(int));
+    int* wordTopicTable = (int*) calloc(numWords * numTopics, sizeof(int)); // vector<vector<int>> wordTopicTable(numWords, vector<int>(numTopics, 1));
+    int* topicTable = (int*) calloc(numTopics, sizeof(int)); // vector<int> topicTable(numTopics, 0);
+    double* p = (double*) calloc(numTopics, sizeof(double)); // vector<double> p(numTopics, 0.0);
 
+    int* updateW = (int*) calloc(numWords * numTopics, sizeof(int));
+    int* updateT = (int*) calloc(numTopics, sizeof(int));
+    int* globalW;
+    int* globalT;
+
+#if MPI
+    globalW = (int*) calloc(numWords * numTopics, sizeof(int));
+    globalT = (int*) calloc(numTopics, sizeof(int));
+#endif
+
+    memset(z, -1, sizeof(int) * TOTAL_WORDS);
+    // memset(docTopicTable, 0, sizeof(int) * numDocs * numTopics);
+    // memset(wordTopicTable, 0, sizeof(int) * numWords * numTopics);
+    // memset(topicTable, 0, sizeof(int) * numTopics);
+    // memset(p, 0.0, sizeof(double) * numTopics);
+
+    int mpi_master = process_id == 0;
+
+    /*
     for (int i = 0; i < numDocs; i++) { // numDocs = w.size();
         vector<int> z_line(w[i].size(), -1);
         z.push_back(z_line);
     }
+    */
 
-    for (int d = 0; d < numDocs; d++) {
-        for (int j = 0; j < w[d].size(); j++) {
-            int word = w[d][j];
+    for (int d = process_id; d < numDocs; d += process_count) {
+        int doffset = d * numTopics;
+        for (int j = w_start[d]; j < w_start[d + 1]; j++) {
+            int word = w[j];
             int topic = rand() % numTopics;
-            z[d][j] = topic;
-            docTopicTable[d][topic]++;
-            wordTopicTable[word][topic]++;
+            z[j] = topic;
+            docTopicTable[doffset + topic]++;
+            wordTopicTable[word * numTopics + topic]++;
             topicTable[topic]++;
         }
     }
@@ -90,20 +114,25 @@ void runLDA(vector<vector<int>> w, vector<vector<int>> &z,
 
         start = clock();
 
-        for (int d = 0; d < numDocs; d++) {
-            for (int j = 0; j < w[d].size(); j++) {
-                int word = w[d][j];
-                int topic = z[d][j];
-                docTopicTable[d][topic]--;
-                wordTopicTable[word][topic]--;
-                topicTable[topic]--;
+        memset(updateW, 0, sizeof(int) * numWords * numTopics);
+        memset(updateT, 0, sizeof(int) * numTopics);
+
+        for (int d = process_id; d < numDocs; d += process_count) {
+            int doffset = d * numTopics;
+            for (int j = w_start[d]; j < w_start[d + 1]; j++) {
+                int word = w[j];
+                int topic = z[j];
+                int woffset = word * numTopics;
+                docTopicTable[doffset + topic]--;
+                updateW[woffset + topic]--; // wordTopicTable[woffset + topic]--;
+                updateT[topic]--; // topicTable[topic]--;
 
                 double norm = 0.0;
                 int newk = topic;
                 for (int k = 0; k < numTopics; k++) {
                     int z_dj_equals_k = (k == topic);
-                    double ak = docTopicTable[d][k] - z_dj_equals_k + alpha;
-                    double bk = (wordTopicTable[word][k] - z_dj_equals_k + beta) / (topicTable[k] - z_dj_equals_k + numWords + beta);
+                    double ak = docTopicTable[doffset + k] - z_dj_equals_k + alpha;
+                    double bk = (wordTopicTable[woffset + k] + updateW[woffset + k] - z_dj_equals_k + beta) / (topicTable[k] + updateT[k] - z_dj_equals_k + numWords + beta);
                     norm += ak * bk;
                     p[k] = norm;
                 }
@@ -142,42 +171,78 @@ void runLDA(vector<vector<int>> w, vector<vector<int>> &z,
                 //         break;
                 //     }
                 // }
-                z[d][j] = newk;
-                docTopicTable[d][newk]++;
-                wordTopicTable[word][newk]++;
-                topicTable[newk]++;
+                z[j] = newk;
+                docTopicTable[doffset + newk]++;
+                updateW[woffset + newk]++; // wordTopicTable[woffset + newk]++;
+                updateT[newk]++; // topicTable[newk]++;
             }
         }
+
+#if MPI
+        MPI_Reduce(updateW, globalW, numWords * numTopics, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(updateT, globalT, numTopics, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+#else 
+        globalW = updateW;
+        globalT = updateT;
+#endif
+        for (int i = 0; i < numWords * numTopics; i++) {
+            wordTopicTable[i] += globalW[i];
+        }
+        for (int i = 0; i < numTopics; i++) {
+            topicTable[i] += globalT[i];
+        }
+
+#if MPI
+        MPI_Bcast(wordTopicTable, numWords * numTopics, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(topicTable, numTopics, MPI_INT, 0, MPI_COMM_WORLD);
+#endif
 
         duration += (clock() - start) / (double)CLOCKS_PER_SEC;
 
         // Output log likelihood at each iteration
-        double lik = getLogLikelihood(wordTopicTable, docTopicTable, alpha, beta);
-        cout << lik << endl;
-    }
-
-    // Output top words for each topic
-    vector<string> vocab;
-    ifstream infile("./data/vocab.csv");
-    string line;
-    while (getline(infile, line))
-    {
-        vocab.push_back(line);
-    }
-    vector<vector<int>> outputTable(wordTopicTable[0].size(), vector<int>(numWords));
-    for (int i = 0; i < numWords; i++) {
-        for (int j = 0; j < wordTopicTable[0].size(); j++) {
-            outputTable[j][i] = wordTopicTable[i][j];
+        if (mpi_master) {
+            double lik = getLogLikelihood(wordTopicTable, docTopicTable, alpha, beta, numWords, numDocs, numTopics);
+            cout << lik << endl;
         }
     }
-    for (int t = 0; t < numTopics; t++) {
-        vector<size_t> v = sort_indexes(outputTable[t]);
-        for (int w = 0; w < 5; w++) {
-            cout << vocab[v[w]] << " ";
+
+    if (mpi_master) {
+        // Output top words for each topic
+        vector<string> vocab;
+        ifstream infile("./data/vocab.csv");
+        string line;
+        while (getline(infile, line))
+        {
+            vocab.push_back(line);
         }
-        cout << endl;
+        vector<vector<int>> outputTable(numTopics, vector<int>(numWords));
+        for (int i = 0; i < numWords; i++) {
+            for (int j = 0; j < numTopics; j++) {
+                outputTable[j][i] = wordTopicTable[i * numTopics + j];
+            }
+        }
+        for (int t = 0; t < numTopics; t++) {
+            vector<size_t> v = sort_indexes(outputTable[t]);
+            for (int w = 0; w < 5; w++) {
+                cout << vocab[v[w]] << " ";
+            }
+            cout << endl;
+        }
+
+        // Output duration of training
+        cout << "Duration of " << numIterations << " Iterations: " << duration << endl;
     }
 
-    // Output duration of training
-    cout << "Duration of " << numIterations << " Iterations: " << duration << endl;
+    free(z);
+    free(p);
+    free(docTopicTable);
+    free(wordTopicTable);
+    free(topicTable);
+    free(updateW);
+    free(updateT);
+#if MPI
+    free(globalW);
+    free(globalT);
+#endif
+
 }
--- main.cpp
+++ main.cpp
@@ -12,31 +12,45 @@
 #include <string>
 #include <vector>
 
+#define TOTAL_WORDS 1414350
+#define	NUM_DOCS 18774
+#define NUM_WORDS 60057
+#define NUM_TOPICS 20
+
 #include "lda.cpp"
 
 using namespace std;
 
-void readCorpus(vector<vector<int>> &w, string filename, int part, int numParts) {
+void readCorpus(int *w, int *w_start, string filename, int part, int numParts) {
+
 	ifstream infile(filename);
 	string line;
-    int count = 0;
+    // int count = 0;
+    int num = 0;
+    int doc = 0;
 	while (getline(infile, line))
 	{
-        if (count == part) {
+        // if (count == part) {
+            w_start[doc] = num;
     	    stringstream ss(line);
-		    vector<int> result;
+		    // vector<int> result;
 		    int i;
 		    while (ss >> i)
     	    {
-        	    result.push_back(i);
+        	    // result.push_back(i);
+                w[num] = i;
+                num++;
         	    if (ss.peek() == ',')
             	    ss.ignore();
-    	        }
-    	    w.push_back(result);
-        }
-        count++;
-        if (count == numParts) count = 0;
+    	    }
+    	    // w.push_back(result);
+            doc++;
+        // }
+        // count++;
+        // if (count == numParts) count = 0;
 	}
+    w_start[doc] = num;
+    cout << "Total number of words: " << num << endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -49,33 +63,43 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &process_count);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
 #endif
-
-	vector<vector<int>> w, z;
-
-	string filename = "20news.csv";
-	readCorpus(w, "./data/" + filename, process_id, process_count);
-
-	int numDocs = w.size();
-	int numWords = 60057; // numWords for 20news.csv
-	int numTopics = 20; // numTopics
-	double alpha = 0.1; // alpha
-	double beta = 0.1; // beta
-	int numIterations = 10; // numIterations
-	// int numClocksPerIteration = 25; // numClocksPerIteration
-	// int staleness = 0; // staleness
     
-    cout << "numDocs = " << numDocs << endl;
-
     bool mpi_master = process_id == 0;
     if (mpi_master) {
         cout << "process count: " << process_count << endl;
     }
 
-	runLDA(w, z, numDocs, numWords, numTopics, alpha, beta, numIterations);
+	// vector<vector<int>> w, z;
+    int* w = (int*)calloc(TOTAL_WORDS, sizeof(int));
+    // int z[TOTAL_WORDS];
+    int* w_start = (int*)calloc(NUM_DOCS + 1, sizeof(int));
+	int numDocs = NUM_DOCS;
+	int numWords = NUM_WORDS;
+	int numTopics = NUM_TOPICS;
+	double alpha = 0.1; // alpha
+	double beta = 0.1; // beta
+	int numIterations = 10; // numIterations
+	// int numClocksPerIteration = 25; // numClocksPerIteration
+	// int staleness = 0; // staleness
+
+    if (mpi_master) {
+	    string filename = "20news.csv";
+	    readCorpus(w, w_start, "./data/" + filename, process_id, process_count);
+    }
+    
+#if MPI
+    MPI_Bcast(w, TOTAL_WORDS, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(w_start, NUM_DOCS + 1, MPI_INT, 0, MPI_COMM_WORLD);
+#endif
+
+	runLDA(w, w_start, numDocs, numWords, numTopics, alpha, beta, numIterations, process_id, process_count);
 
 #if MPI
     MPI_Finalize();
 #endif
+
+    free(w);
+    free(w_start);
 
 	return 0;
 }
