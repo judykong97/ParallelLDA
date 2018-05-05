8b9debbecbede54e1b43b0f3bcf432b4c38db6a7
junhank <junhank@andrew.cmu.edu>
2018-05-05 17:30:31 -0400

    Experimenting script

---
+++ exp_proc.sh
@@ -0,0 +1,9 @@
+./run.sh -p 1 -i 1000 -s 1
+./run.sh -p 2 -i 1000 -s 1
+./run.sh -p 3 -i 1000 -s 1
+./run.sh -p 4 -i 1000 -s 1
+./run.sh -p 5 -i 1000 -s 1
+./run.sh -p 6 -i 1000 -s 1
+./run.sh -p 7 -i 1000 -s 1
+./run.sh -p 8 -i 1000 -s 1
+
---
+++ exp_stale.sh
@@ -0,0 +1,4 @@
+./run.sh -p 4 -i 1000 -s 1
+./run.sh -p 4 -i 1000 -s 2
+./run.sh -p 4 -i 1000 -s 3
+./run.sh -p 4 -i 1000 -s 4
